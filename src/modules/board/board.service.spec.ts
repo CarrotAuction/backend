138df5c13796262894809e413b3f317e80a01c64
ppinkypeach <mira20932@gmail.com>
2023-11-04 18:26:15 +0900

    feat: board unit test 기본 mocking 설정

--- src/modules/board/board.service.spec.ts
+++ src/modules/board/board.service.spec.ts
@@ -1,12 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BoardService } from './board.service';
+import { Repository } from 'typeorm';
+import { BoardMapper } from './mapper/board.mapper';
+import { Board } from './entity/board.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+
+type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>; 
+type MockMapper = Partial<Record<keyof BoardMapper, jest.Mock>>;
 
 describe('BoardService', () => {
   let service: BoardService;
+  let boardRepository: MockRepository<Board>;
+  let boardMapper: MockMapper;
+
+  const mockRepository = () => ({
+    save: jest.fn(),
+  });
+
+  const mockBoardMapper = {
+    DtoToEntity: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [BoardService],
+      providers: [
+        BoardService,
+        {
+          provide: getRepositoryToken(Board),
+          useValue: mockRepository(),
+        },
+        {
+          provide: BoardMapper,
+          useValue: mockBoardMapper,
+        },
+      ],
     }).compile();
 
     service = module.get<BoardService>(BoardService);
