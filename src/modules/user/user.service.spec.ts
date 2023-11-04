1d7893193b1d72bb92bdb7e40860ef944fa7b670
ppinkypeach <mira20932@gmail.com>
2023-11-04 17:57:30 +0900

    refactor: user unit test에 queryBuilder 추가

--- src/modules/user/user.service.spec.ts
+++ src/modules/user/user.service.spec.ts
@@ -8,35 +8,54 @@ import { NicknameAlreadyExistsException } from './userException/NicknameAlreadyE
 import { NotFoundUserException } from './userException/NotFoundUserException';
 import { LoginInvalidPasswordException } from './userException/LoginInvalidPasswordException';
 import { Repository } from 'typeorm';
-
-const mockRepository = () => ({
-  findOne: jest.fn(),
-  save: jest.fn(),
-})
-
-const mockUserMapper = {
-  DtoToEntity: jest.fn(),
-}
+import { City } from '../location/entity/city.entity';
+import { Province } from '../location/entity/province.entity';
+import { ProvinceInvalidException } from './userException/ProvinceInvalidException';
+import { CityInvalidException } from './userException/CityInvalidException';
 
 type MockRepository<T = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
-
 type MockMapper = Partial<Record<keyof UserMapper, jest.Mock>>;
 
 describe('UserService Unit Test', () => {
   let service: UserService;
   let userRepository: MockRepository<User>;
+  let provinceRepository: MockRepository<Province>;
+  let cityRepository: MockRepository<City>;
   let userMapper: MockMapper;
 
   const mockDto = {
     email: 'test@test.com',
-    password: 'test',
-    nickname: 'test',
-    location: 'test',
+    password: '12345',
+    nickname: '사용자',
+    province: '서울특별시',
+    city: '강남구',
   };
+
+  const mockQueryBuilder = {
+    leftJoin: jest.fn().mockReturnThis(),
+    where: jest.fn().mockReturnThis(),
+    andWhere: jest.fn().mockReturnThis(),
+    getOne: jest.fn(),
+  }
+
+  const mockUserEntity = Object.assign(new User(), mockDto);
 
   const mockLoginDto = {
     nickname: 'test-nickname',
     password: 'test-correctPassword',
+  }
+
+  const mockProvince = { id: 1, name: mockDto.province };
+  const mockCity = { id: 1, name: mockDto.city, province_id: mockProvince.id };
+
+  const mockRepository = () => ({
+    findOne: jest.fn(),
+    save: jest.fn(),
+    createQueryBuilder: jest.fn(),
+  });
+
+  const mockUserMapper = {
+    DtoToEntity: jest.fn(),
   };
 
   beforeEach(async () => {
@@ -45,51 +64,80 @@ describe('UserService Unit Test', () => {
         UserService,
         {
           provide: getRepositoryToken(User),
-          useValue: mockRepository()
+          useValue: mockRepository(),
+        },
+        {
+          provide: getRepositoryToken(Province),
+          useValue: mockRepository(),
+        },
+        {
+          provide: getRepositoryToken(City),
+          useValue: mockRepository(),
         },
         {
           provide: UserMapper,
-          useValue: mockUserMapper
+          useValue: mockUserMapper,
         },
       ],
     }).compile();
 
     service = module.get<UserService>(UserService);
     userRepository = module.get(getRepositoryToken(User));
-    userMapper = mockUserMapper;
+    provinceRepository = module.get(getRepositoryToken(Province));
+    cityRepository = module.get(getRepositoryToken(City));
+    userMapper = module.get(UserMapper);
+    cityRepository.createQueryBuilder.mockReturnValue(mockQueryBuilder);
 
   });
 
   afterEach(() => {
-    userRepository.findOne.mockClear();
-    userRepository.save.mockClear();
-    userMapper.DtoToEntity.mockClear();
+    jest.clearAllMocks();
   });
-
 
   it('SUCCESS: 사용자는 회원가입을 한다', async () => {
 
-    const mockUserEntity = Object.assign(new User(), mockDto);
-
-    userRepository.findOne
-      .mockResolvedValueOnce(null)
-      .mockResolvedValueOnce(null);
-
-
+    provinceRepository.findOne.mockResolvedValueOnce(mockProvince);
+    mockQueryBuilder.getOne.mockResolvedValue(mockCity);
+    userRepository.findOne.mockResolvedValueOnce(null).mockResolvedValueOnce(null);
     userMapper.DtoToEntity.mockReturnValue(mockUserEntity);
-
     userRepository.save.mockResolvedValue(mockUserEntity);
 
     const result = await service.registerUser(mockDto);
-    
+
+    // 결과 값이 같은 지 검증
     expect(result).toEqual(mockUserEntity);
-    expect(userRepository.findOne).toHaveBeenCalledWith({where: {email: mockDto.email}});
-    expect(userRepository.findOne).toHaveBeenCalledWith({where: {nickname: mockDto.nickname}});
+
+    // province 검증
+    expect(provinceRepository.findOne).toHaveBeenCalledWith({ where: { name: mockDto.province } });
+
+    // city QueryBuilder 함수 호출 검증
+    expect(cityRepository.createQueryBuilder).toHaveBeenCalled();
+    expect(cityRepository.createQueryBuilder().leftJoin).toHaveBeenCalledWith(
+      "province", "province", "city.province_id = province.id"
+    );
+    expect(cityRepository.createQueryBuilder().where).toHaveBeenCalledWith(
+      "city.name = :cityName", { cityName: mockDto.city }
+    );
+    expect(cityRepository.createQueryBuilder().andWhere).toHaveBeenCalledWith(
+      "province.id = :provinceId", { provinceId: mockProvince.id }
+    );
+    expect(cityRepository.createQueryBuilder().getOne).toHaveBeenCalled();
+
+    // userRepository 실행 순서 검증
+    expect(userRepository.findOne).toHaveBeenNthCalledWith(1, { where: { email: mockDto.email } });
+    expect(userRepository.findOne).toHaveBeenNthCalledWith(2, { where: { nickname: mockDto.nickname } });
+
+    // userRepository 호출 횟수 검증
     expect(userRepository.findOne).toHaveBeenCalledTimes(2);
+
+    // save 호출 횟수, 조건 검증
     expect(userRepository.save).toHaveBeenCalledTimes(1);
     expect(userRepository.save).toHaveBeenCalledWith(mockUserEntity);
+
+    // DtoToEntity 호출 횟수, 조건 검증
     expect(userMapper.DtoToEntity).toHaveBeenCalledTimes(1);
-    expect(userMapper.DtoToEntity).toHaveBeenCalledWith(mockDto);
+    expect(userMapper.DtoToEntity).toHaveBeenCalledWith(mockDto, mockProvince, mockCity);
+
   });
 
 
@@ -107,8 +155,36 @@ describe('UserService Unit Test', () => {
     expect(result).toBe(`${mockLoginDto.nickname}님 안녕하세요!`);
   });
 
+  it('ERROR: 회원가입 시 없는 행정구역을 선택하면 ProvinceInvalidException을 반환', async () => {
+
+    provinceRepository.findOne
+      .mockResolvedValue(null)
+
+    await expect(service.registerUser(mockDto))
+      .rejects.toThrow(ProvinceInvalidException);
+    
+    expect(provinceRepository.findOne).toHaveBeenCalledWith({where: {name: mockDto.province}});
+  });
+
+
+  it('ERROR: 회원가입 시 없는 행정구역을 선택하면 CityInvalidException을 반환', async () => {
+
+    provinceRepository.findOne
+      .mockResolvedValue(mockProvince);
+
+      mockQueryBuilder.getOne.mockResolvedValue(null);
+
+    await expect(service.registerUser(mockDto))
+      .rejects.toThrow(CityInvalidException);
+
+  });
+
 
   it('ERROR: 회원가입 시 중복된 이메일이 있으면 EmailAlreadyExistsException을 반환', async () => {
+
+
+    provinceRepository.findOne.mockResolvedValueOnce(mockProvince);
+    mockQueryBuilder.getOne.mockResolvedValue(mockCity);
 
     userRepository.findOne
       .mockResolvedValue({
@@ -124,6 +200,9 @@ describe('UserService Unit Test', () => {
 
 
   it('ERROR: 회원가입 시 중복된 닉네임이 있으면 NicknameAlreadyExistsException을 반환', async () => {
+
+    provinceRepository.findOne.mockResolvedValueOnce(mockProvince);
+    mockQueryBuilder.getOne.mockResolvedValue(mockCity);
 
     userRepository.findOne
       .mockResolvedValueOnce(null)
