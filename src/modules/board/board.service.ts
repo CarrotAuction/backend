f3cfab1373d862538392883367ba844ee2fbf3f3
ppinkypeach <mira20932@gmail.com>
2023-11-07 23:32:43 +0900

    refactor: customRespository로 service와 repository분할

--- src/modules/board/board.service.ts
+++ src/modules/board/board.service.ts
@@ -7,94 +7,36 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../user/entity/user.entity';
 import { NotFoundUserException } from '../user/userException/NotFoundUserException';
 import { BoardPaginationReqestDto } from './dto/board-pagination-request.dto';
+import { BoardRepository } from './repository/boardRepository';
 
 @Injectable()
 export class BoardService {
 
     constructor(
-        @InjectRepository(Board)
-        private readonly boardRepository: Repository<Board>,
+        private readonly boardRepository: BoardRepository,
 
         @InjectRepository(User)
         private readonly userRepository: Repository<User>,
 
         private readonly boardMapper: BoardMapper
     ) {}
-    
 
     async createBoard(createBoardRequestDto: CreateBoardRequestDto): Promise<Board> {
-
         const creator = await this.userRepository.findOne({where: {id: createBoardRequestDto.creatorId}});
         if(!creator){
             throw new NotFoundUserException();
         }
-
         const newBoardEntity = this.boardMapper.DtoToEntity(createBoardRequestDto, creator);
         return await this.boardRepository.save(newBoardEntity);
     }
 
 
     async getBoardDetail(boardId: number): Promise<Board>{
-        return await this.boardRepository.createQueryBuilder('board')
-                .leftJoin('board.creator', 'user')
-                .leftJoin('user.province', 'province')
-                .leftJoin('user.city', 'city')
-                .select([
-                    'board.id',
-                    'board.stuffName',
-                    'board.stuffContent',
-                    'board.stuffPrice',
-                    'board.stuffCategory',
-                    'user.nickname',
-                    'province.name',
-                    'city.name'
-                ])
-                .where('board.id =:id', {id: boardId})
-                .getOne();
+        return await this.boardRepository.findBoardById(boardId);
     }
 
+
     async getAllBoard(boardPaginationRequestDto: BoardPaginationReqestDto): Promise<[Board[], number]>{
-
-        const {titleSearch, provinceName, cityName, stuffCategory, limit, offset} = boardPaginationRequestDto;
-        const query = this.boardRepository.createQueryBuilder('board')
-                .leftJoin('board.creator', 'user')
-                .leftJoin('user.province', 'province')
-                .leftJoin('user.city', 'city')
-                .select([
-                    'board.id',
-                    'board.stuffName',
-                    'board.stuffContent',
-                    'board.stuffPrice',
-                    'board.stuffCategory',
-                    'board.createAt',
-                    'user.id',
-                    'province.name',
-                    'city.name'
-                ])
-                .where('board.deleteAt IS NULL');
-
-                if(titleSearch) {
-                    query.andWhere('board.stuffName LIKE :search', { search: `%${titleSearch}%` });
-                }
-
-                if(provinceName) {
-                    query.andWhere('province.name = :provinceName', {provinceName});
-                }
-
-                if(cityName) {
-                    query.andWhere('city.name = :cityName', {cityName});
-                }
-
-                if(stuffCategory) {
-                    query.andWhere('board.stuffCategory = :stuffCategory', {stuffCategory});
-                }
-
-                query.orderBy('board.createAt', 'DESC')
-                    .addOrderBy('board.id', 'ASC')
-                    .offset(offset)
-                    .limit(limit)
-
-            return await query.getManyAndCount();
-            
-            }
+        return await this.boardRepository.findAllBoard(boardPaginationRequestDto);        
+    }
 }
---
+++ src/modules/board/repository/boardRepository.ts
@@ -0,0 +1,78 @@
+import { DataSource, Repository } from "typeorm";
+import { Board } from "../entity/board.entity";
+import { Injectable } from "@nestjs/common";
+import { BoardPaginationReqestDto } from "../dto/board-pagination-request.dto";
+
+@Injectable()
+export class BoardRepository extends Repository<Board> {
+
+    constructor(dataSource: DataSource){
+        super(Board, dataSource.createEntityManager());
+    }
+
+    async findBoardById(boardId: number): Promise<Board> { 
+        return await this.createQueryBuilder('board')
+            .leftJoin('board.creator', 'user')
+            .leftJoin('user.province', 'province')
+            .leftJoin('user.city', 'city')
+            .select([
+                'board.id',
+                'board.stuffName',
+                'board.stuffContent',
+                'board.stuffPrice',
+                'board.stuffCategory',
+                'user.nickname',
+                'province.name',
+                'city.name'
+            ])
+            .where('board.id =:id', {id: boardId})
+            .getOne();
+    }
+
+
+    async findAllBoard(boardPaginationRequestDto: BoardPaginationReqestDto): Promise<[Board[], number]> {
+        const {titleSearch, provinceName, cityName, stuffCategory, limit, offset} = boardPaginationRequestDto;
+        const query = this.createQueryBuilder('board')
+                .leftJoin('board.creator', 'user')
+                .leftJoin('user.province', 'province')
+                .leftJoin('user.city', 'city')
+                .select([
+                    'board.id',
+                    'board.stuffName',
+                    'board.stuffContent',
+                    'board.stuffPrice',
+                    'board.stuffCategory',
+                    'board.createAt',
+                    'user.id',
+                    'province.name',
+                    'city.name'
+                ])
+                .where('board.deleteAt IS NULL');
+
+                if(titleSearch) {
+                    query.andWhere('board.stuffName LIKE :search', { search: `%${titleSearch}%` });
+                }
+
+                if(provinceName) {
+                    query.andWhere('province.name = :provinceName', {provinceName});
+                }
+
+                if(cityName) {
+                    query.andWhere('city.name = :cityName', {cityName});
+                }
+
+                if(stuffCategory) {
+                    query.andWhere('board.stuffCategory = :stuffCategory', {stuffCategory});
+                }
+
+                query.orderBy('board.createAt', 'DESC')
+                    .addOrderBy('board.id', 'ASC')
+                    .offset(offset)
+                    .limit(limit)
+
+            return await query.getManyAndCount();
+            
+            }
+
+
+}
