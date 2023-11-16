a1f52df87dc8ce202399add99abcecdab9b9ffad
ppinkypeach <mira20932@gmail.com>
2023-11-16 17:16:10 +0900

    fix: 로그인 시 dto가 Null로 바뀌는 에러 해결

--- src/modules/user/dto/user-login.dto.ts
+++ src/modules/user/dto/user-login.dto.ts
@@ -1,6 +1,13 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class LoginUserDto {
     
+    @IsString()
+    @IsNotEmpty()
     email: string;
+
+    @IsString()
+    @IsNotEmpty()
     password: string;
 
 }
