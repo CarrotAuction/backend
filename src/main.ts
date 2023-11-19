028992e65aaa6f21004012abc5c3deae2eef3fcb
ppinkypeach <mira20932@gmail.com>
2023-11-19 17:22:02 +0900

    chore: cors 설정

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: 'http://localhost:3000', 
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+      credentials: true, 
+    },
+  });
+  
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
