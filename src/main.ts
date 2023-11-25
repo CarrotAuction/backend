8d818058a34547055c31da11536e5a18f37a1412
희은 <datakubit2@gmail.com>
2023-11-25 19:06:54 +0900

    feat: swagger 문서화

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { initializeTransactionalContext } from 'typeorm-transactional';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { BoardModule } from './modules/board/board.module';
 import { CommentModule } from './modules/comment/comment.module';
 import { UserModule } from './modules/user/user.module';
 
 async function bootstrap() {
-  initializeTransactionalContext();
-
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: 'http://localhost:3000',
