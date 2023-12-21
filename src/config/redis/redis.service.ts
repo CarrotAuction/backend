aabfcb02f661caa0d8ed4f17ff57e2cdf10c5983
ppinkypeach <mira20932@gmail.com>
2023-12-21 22:19:59 +0900

    refactor: redisService 변경

--- src/config/redis/redis.service.ts
+++ src/config/redis/redis.service.ts
@@ -10,25 +10,45 @@ export class RedisService {
         this.redisClient = redisService.getClient();
     }
 
-    async setValues(key: string, data: string): Promise<string>{
-        await this.redisClient.set(key, data);
-        return `Redis에 key가 생성됨`;
+    // async setValues(key: string, data: string): Promise<string>{
+    //     await this.redisClient.set(key, data);
+    //     return `Redis에 key가 생성됨`;
+    // }
+
+    // async setValuesList(key: string, data: string): Promise<number>{
+    //     return await this.redisClient.rpush(key, data);
+    // }
+
+    // async getValues(key: string): Promise<string|null>{
+    //     return await this.redisClient.get(key);
+    // }
+
+    // async getValuesList(key: string): Promise<string[]>{
+    //     const len = await this.redisClient.llen(key);
+    //     return len === 0 ? [] : await this.redisClient.lrange(key, 0, len-1);
+    // }
+
+    // async deleteValuesList(key: string, data: string): Promise<number>{
+    //     return await this.redisClient.lrem(key, 0, data);
+    // }
+
+    async boardLikesInc(key: string): Promise<number>{
+        return await this.redisClient.incr(key);
     }
 
-    async setValuesList(key: string, data: string): Promise<number>{
-        return await this.redisClient.rpush(key, data);
+    async boardLikesDec(key: string): Promise<number>{
+        return await this.redisClient.decr(key);
     }
 
-    async getValues(key: string): Promise<string|null>{
-        return await this.redisClient.get(key);
+    async addUserLikesSet(key: string, value: string): Promise<number>{
+        return await this.redisClient.sadd(key, value);
     }
 
-    async getValuesList(key: string): Promise<string[]>{
-        const len = await this.redisClient.llen(key);
-        return len === 0 ? [] : await this.redisClient.lrange(key, 0, len-1);
+    async removeUserLikesSet(key: string, value: string): Promise<number>{
+        return await this.redisClient.srem(key, value);
     }
 
-    async deleteValuesList(key: string, data: string): Promise<number>{
-        return await this.redisClient.lrem(key, 0, data);
+    async isUserIncludeSet(key: string, value: string): Promise<boolean>{
+        return await this.redisClient.sismember(key, value) ===1 ;
     }
 }
