56f747aba3676fc713ea1530ed056bec453144e8
ppinkypeach <mira20932@gmail.com>
2024-03-07 16:01:52 +0900

    feat: passport strategy 구현

---
+++ src/modules/auth/strategies/user-local.auth.strategy.ts
@@ -0,0 +1,22 @@
+import { PassportStrategy } from "@nestjs/passport";
+import { Strategy } from "passport-local";
+import { AuthService } from "../auth.service";
+import { UserAuthResult } from "src/interfaces/user-auth-result.interface";
+import { LoginInvalidPasswordException } from "../authException/LoginInvalidPasswordException";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class UserLocalStrategy extends PassportStrategy(Strategy, 'user-local'){
+    constructor(private authService: AuthService){
+        super({usernameField: 'accountID', passwordField: 'password'});
+    }
+
+    async validate(accountID: string, password: string): Promise<UserAuthResult>{
+        const user = await this.authService.validateUser({accountID, password});
+        if(!user){
+            throw new LoginInvalidPasswordException();
+        }
+        return user;
+    }
+}
+
